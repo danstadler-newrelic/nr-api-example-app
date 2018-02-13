82feeb4bc14f5bec015a019b06d187594671751a
Dan Stadler <danstadler@gmail.com>
2018-02-13 12:49:43 -0800

    change

--- grails-app/services/nr/billing/usage/InsightsService.groovy
+++ grails-app/services/nr/billing/usage/InsightsService.groovy
@@ -6,52 +6,6 @@ class InsightsService {
 
     def insightsApi = "https://insights-api.newrelic.com/v1/accounts"
 
-    // insights doesn't have a paging feature like the REST api does, but you still
-    // might want to page out rows at a time, and then work with timestamps as 
-    // page markers
-    def insightsPagedData(Integer accountId, String insightsKey, String query, Integer pageSize, Integer maxPages) {
-
-      RestBuilder rest = new RestBuilder()
-      def response = []
-
-      def nextPage = "${insightsApi}/${String.valueOf(accountId)}/query?nrql={nrql}&source_id={source_id}"
-      def initialQuery = "${query} LIMIT ${pageSize}" 
-      def qp = [
-        nrql: initialQuery,
-        source_id: "${String.valueOf(accountId)}"
-      ]
-
-      def lastTimestamp = -1
-      def pageNumber = 0
-
-      println "======== calling data: ========="
-      while (pageNumber < maxPages && nextPage && nextPage != "" && nextPage.startsWith("https://insights-api.newrelic.com")) {
-        println "page: ${pageNumber}"
-        println nextPage
-        println qp["nrql"]
-        def resp
-        resp = rest.get(nextPage, qp) {
-          header 'X-Query-Key', "${insightsKey}"
-        }
-        println "----- status code: ${resp.statusCode}"
-        //println resp.body
-
-        resp.json.results.events[0].each {
-          lastTimestamp = it.timestamp
-          response.add(it)
-        }
-
-        // try paging by timestamps here
-        def nextQuery = "${initialQuery} WHERE timestamp < ${lastTimestamp}"
-        qp["nrql"] = nextQuery
-
-        pageNumber++
- 
-      }
-      response
-    }
-
-
     def insightsFacetedData(Integer accountId, String insightsKey, String query){
       RestBuilder rest = new RestBuilder()
       def response = []
